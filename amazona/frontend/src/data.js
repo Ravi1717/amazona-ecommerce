0cc94daf8f8ec4952a51d94dc96b16245d4c3fea
ravi1717 <vermarv0802@gmail.com>
2020-06-28 12:38:32 +0530

    product data changes static to dynamic

---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,31 @@
+export default {
+  products: [
+    {
+      name: "Slim Shirt",
+      category: "Shirts",
+      image: "images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 3.5,
+      numReviews: 21,
+    },
+    {
+      name: "Fit Shirt",
+      category: "Shirts",
+      image: "images/d1.jpg",
+      price: 50,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      name: "Best Pants",
+      category: "Pants",
+      image: "images/d1.jpg",
+      price: 70,
+      brand: "Nike",
+      rating: 4.0,
+      numReviews: 8,
+    },
+  ],
+};
