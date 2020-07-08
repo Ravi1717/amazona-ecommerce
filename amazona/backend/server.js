404e7b341ec9bcfadaaf859045c03e40ece94e4f
ravi1717 <vermarv0802@gmail.com>
2020-07-08 20:23:03 +0530

    successfully added redux in product detail and also for cross-origin cors is install in backend

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -2,10 +2,21 @@ import express from "express";
 
 import data from "./data";
 
+import cors from "cors";
+
 const app = express();
+
+app.use(cors());
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = data.products.find((x) => x._id == productId);
+  if (product) res.send(product);
+  else res.status(404).send({ msg: " Product not Found" });
 });
 
 app.listen(5000, () => {
