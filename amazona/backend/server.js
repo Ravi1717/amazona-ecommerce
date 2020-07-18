cd54b7d2f411601e7606c87e296acdc440cdd0ef
ravi1717 <vermarv0802@gmail.com>
2020-07-19 00:36:04 +0530

    mongoDB local connection stablised

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -7,13 +7,26 @@ import cors from "cors";
 import dotenv from "dotenv";
 import config from "./config";
 
+import mongoose from "mongoose";
+
+import userRoute from "./routes/userRoutes";
+
 dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
 
 const app = express();
 
 app.use(cors());
+
+app.use("/api/users", userRoute);
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
