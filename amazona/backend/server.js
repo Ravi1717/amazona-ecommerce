17678ef6f9bfb4d91b3379ead8627d08499a4489
ravi1717 <vermarv0802@gmail.com>
2020-07-30 03:22:00 +0530

    creating product screen  added in frontend

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -13,6 +13,8 @@ import bodyParser from "body-parser";
 
 import userRoute from "./routes/userRoutes";
 
+import productRoute from "./routes/productRoute";
+
 dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
@@ -31,17 +33,18 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
+// app.get("/api/products", (req, res) => {
+//   res.send(data.products);
+// });
 
-app.get("/api/products/:id", (req, res) => {
-  const productId = req.params.id;
-  const product = data.products.find((x) => x._id == productId);
-  if (product) res.send(product);
-  else res.status(404).send({ msg: " Product not Found" });
-});
+// app.get("/api/products/:id", (req, res) => {
+//   const productId = req.params.id;
+//   const product = data.products.find((x) => x._id == productId);
+//   if (product) res.send(product);
+//   else res.status(404).send({ msg: " Product not Found" });
+// });
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
