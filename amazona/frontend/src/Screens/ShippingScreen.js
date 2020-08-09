8b861eb00305e6440bd699d6152d5deb55b28b4d
ravi1717 <vermarv0802@gmail.com>
2020-08-09 22:46:13 +0530

    shipping screen part done in frontend

--- amazona/frontend/src/Screens/ShippingScreen.js
+++ amazona/frontend/src/Screens/ShippingScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { saveShipping } from "../actions/cartAction";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 function ShippingScreen(props) {
   const [address, setAddress] = useState("");
@@ -14,60 +15,64 @@ function ShippingScreen(props) {
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(saveShipping(address, city, postalCode, country));
+    props.history.push("payment");
   };
 
   return (
-    <div className="form">
-      <form onSubmit={submitHandler}>
-        <ul className="form-container">
-          <li>
-            <h2>Shipping</h2>
-          </li>
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <ul className="form-container">
+            <li>
+              <h2>Shipping</h2>
+            </li>
 
-          <li>
-            <label htmlFor="address">Address</label>
-            <input
-              type="text"
-              address="address"
-              id="address"
-              onChange={(e) => setAddress(e.target.value)}
-            ></input>
-          </li>
-          <li>
-            <label htmlFor="city">City</label>
-            <input
-              type="text"
-              address="city"
-              id="city"
-              onChange={(e) => setCity(e.target.value)}
-            ></input>
-          </li>
-          <li>
-            <label htmlFor="postalCode">Postal Code</label>
-            <input
-              type="text"
-              address="postalCode"
-              id="postalCode"
-              onChange={(e) => setPostalCode(e.target.value)}
-            ></input>
-          </li>
-          <li>
-            <label htmlFor="country">Country</label>
-            <input
-              type="text"
-              address="country"
-              id="country"
-              onChange={(e) => setCountry(e.target.value)}
-            ></input>
-          </li>
+            <li>
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                address="address"
+                id="address"
+                onChange={(e) => setAddress(e.target.value)}
+              ></input>
+            </li>
+            <li>
+              <label htmlFor="city">City</label>
+              <input
+                type="text"
+                address="city"
+                id="city"
+                onChange={(e) => setCity(e.target.value)}
+              ></input>
+            </li>
+            <li>
+              <label htmlFor="postalCode">Postal Code</label>
+              <input
+                type="text"
+                address="postalCode"
+                id="postalCode"
+                onChange={(e) => setPostalCode(e.target.value)}
+              ></input>
+            </li>
+            <li>
+              <label htmlFor="country">Country</label>
+              <input
+                type="text"
+                address="country"
+                id="country"
+                onChange={(e) => setCountry(e.target.value)}
+              ></input>
+            </li>
 
-          <li>
-            <button type="submit" className="button primary">
-              Continue
-            </button>
-          </li>
-        </ul>
-      </form>
+            <li>
+              <button type="submit" className="button primary">
+                Continue
+              </button>
+            </li>
+          </ul>
+        </form>
+      </div>
     </div>
   );
 }
