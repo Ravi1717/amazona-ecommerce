fbb2366cc2673ddf635c447b70f89d2a90efcfa0
ravi1717 <vermarv0802@gmail.com>
2020-08-10 02:57:41 +0530

    order history page done

--- amazona/frontend/src/reducers/cartReducers.js
+++ amazona/frontend/src/reducers/cartReducers.js
@@ -5,7 +5,10 @@ import {
   CART_SAVE_PAYMENT,
 } from "../constants/cartConstants";
 
-function cartReducer(state = { cartItems: [] }, action) {
+function cartReducer(
+  state = { cartItems: [], shipping: {}, payment: {} },
+  action
+) {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
